803d8a9ed11441b173ffe7b4926404d5eaae4c55
yudada <yudada0312@gmail.com>
2019-01-27 22:36:45 +0800

    modify base image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:7.2.14-alpine
+FROM php:7.2.14-fpm-alpine3.8
 LABEL maintainer="yudada <yudada0312@gmail.com>"
 
 RUN apk update && apk upgrade && apk add bash git
@@ -9,5 +9,5 @@ RUN docker-php-ext-install pdo_mysql
 RUN docker-php-ext-install bcmath
 RUN chmod u+x /var/entrypoint.sh
 
-WORKDIR /var/www
+WORKDIR /var/www/html
 CMD ["/bin/sh", "-c","/var/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,18 @@ role=${CONTAINER_ROLE:-app}
 
 if [ "$role" = "app" ]; then
 
-    php /var/www/artisan serve --port=80 --host=0.0.0.0
+    php-fpm
 
 elif [ "$role" = "queue" ]; then
 
     echo "Running the queue..."
-    php /var/www/artisan queue:work --verbose --tries=3 --timeout=90
+    php artisan queue:work --verbose --tries=3 --timeout=90
 
 elif [ "$role" = "scheduler" ]; then
 
     while [ true ]
     do
-      php /var/www/artisan schedule:run --verbose --no-interaction &
+      php artisan schedule:run --verbose --no-interaction &
       sleep 5
     done
 
