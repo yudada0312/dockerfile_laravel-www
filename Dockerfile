a101a7976dcdba1b77ffac8e207fb30ed150465f
yudada <yudada0312@gmail.com>
2019-05-02 17:23:21 +0800

    排程調整每分中觸發一次

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM php:7.2.14-fpm-alpine3.8
 LABEL maintainer="yudada <yudada0312@gmail.com>"
 
-RUN apk update && apk upgrade && apk add bash git
+ENV TIME_ZONE Asia/Shanghai
+ENV WORKDIR /var/www/html
+
+RUN apk update && apk upgrade && apk add bash git && apk add --no-cache tzdata && echo "${TIME_ZONE}" > /etc/timezone && ln -sf /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
 COPY php.ini /usr/local/etc/php
 COPY entrypoint.sh /var
 
@@ -10,5 +13,5 @@ RUN docker-php-ext-install pdo_mysql
 RUN docker-php-ext-install bcmath
 RUN chmod u+x /var/entrypoint.sh
 
-WORKDIR /var/www/html
+WORKDIR ${WORKDIR}
 CMD ["/bin/sh", "-c","/var/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,19 +12,11 @@ elif [ "$role" = "queue" ]; then
     php artisan queue:work --verbose --tries=3 --timeout=90
 
 elif [ "$role" = "scheduler" ]; then
-
-    while [ true ]
-    do
-      t=`date +%-S`
-      if [ $(($t%10)) = "0" ]; then
-        date +"%Y-%m-%d %T"
-        php artisan schedule:run &
-        sleep 10
-        else
-        sleep 1
-      fi
-    done
-
+    echo "Running the scheduler..."
+    echo "* * * * *  cd $WORKDIR && php artisan schedule:run >> /var/schedule.log 2>&1" >> /etc/crontabs/root
+    crond
+    cd /var/ && touch schedule.log
+    tail -f /var/schedule.log
 else
     echo "Could not match the container role \"$role\""
     exit 1
